08bc717e3586d6fd1cd18f4382e8abac02464abc
herb13 <herb13sw@gmail.com>
2014-09-12 17:41:36 +0200

    Updated form

--- app/view/comment/form.tpl.php
+++ app/view/comment/form.tpl.php
@@ -2,15 +2,15 @@
     <form method=post>
         <input type=hidden name="redirect" value="<?=$this->url->create('guestbook')?>">
         <fieldset>
-        <legend>Leave a comment</legend>
-        <p><label>Comment:<br/><textarea name='content'><?=$content?></textarea></label></p>
-        <p><label>Name:<br/><input type='text' name='name' value='<?=$name?>'/></label></p>
-        <p><label>Homepage:<br/><input type='text' name='web' value='<?=$web?>'/></label></p>
-        <p><label>Email:<br/><input type='text' name='mail' value='<?=$mail?>'/></label></p>
+        <legend>Skriv en kommentar</legend>
+        <p><label>Kommentar:<br/><textarea name='content'><?=$content?></textarea></label></p>
+        <p><label>Namn:<br/><input type='text' name='name' value='<?=$name?>'/></label></p>
+        <p><label>Hemsida:<br/><input type='text' name='web' value='<?=$web?>'/></label></p>
+        <p><label>E-post:<br/><input type='text' name='mail' value='<?=$mail?>'/></label></p>
         <p class=buttons>
-            <input type='submit' name='doCreate' value='Comment' onClick="this.form.action = '<?=$this->url->create('comment/add')?>'"/>
+            <input type='submit' name='doCreate' value='Spara kommentar' onClick="this.form.action = '<?=$this->url->create('comment/add')?>'"/>
             <input type='reset' value='Reset'/>
-            <input type='submit' name='doRemoveAll' value='Remove all' onClick="this.form.action = '<?=$this->url->create('comment/remove-all')?>'"/>
+            <input type='submit' name='doRemoveAll' value='Radera alla kommentarer' onClick="this.form.action = '<?=$this->url->create('comment/remove-all')?>'"/>
         </p>
         <output><?=$output?></output>
         </fieldset>
